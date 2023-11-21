3a04747330243f46769a96e980fec81e0ee79f13
Gor Beglaryan <142711745+Gor903@users.noreply.github.com>
2023-11-21 23:32:33 +0400

    Changed "paths" and bug fix (#30)
    * feat: updated checkers gql mutations, made fixes [Gor]
    
    * Refactored Queries for Checkers game
    
    ---------
    
    Co-authored-by: Vachagan <vachagan.grigoryan@outlook.com>
    Co-authored-by: saamo24 <sabrahamyan.004@gmail.com>

--- games/checkers/mutations.py
+++ games/checkers/mutations.py
@@ -1,25 +1,22 @@
-import uuid
-from datetime import datetime
 from typing import List
 
 import strawberry
 from asgiref.sync import sync_to_async
 from jwtberry.permission import IsAuthenticated
-from strawberry.field_extensions import InputMutationExtension
-from strawberry.types import Info
-from typing_extensions import Annotated
 
 from .game import init_game, update_board
-from . import models
 from core.json import JSON
 
 
-@strawberry.mutation
-# @login_required
-def create_board(owner: str, color: str, length: int) -> JSON:
-    return init_game(owner, color, length)
+@strawberry.type
+class CheckersMutation:
 
+    @strawberry.mutation(permission_classes=[IsAuthenticated])
+    @sync_to_async
+    def create_board(self, owner: str, color: str, length: int) -> JSON:
+        return init_game(owner, color, length)
 
-@strawberry.mutation#(permission_classes=[IsAuthenticated])
-def update_board_in_game(guid: strawberry.ID, grid_changes: List[List[int]]) -> JSON:
-    return update_board(guid=guid, grid_changes=grid_changes)
+    @strawberry.mutation(permission_classes=[IsAuthenticated])
+    @sync_to_async
+    def update_board_in_game(self, guid: strawberry.ID, grid_changes: List[List[int]]) -> JSON:
+        return update_board(guid=guid, grid_changes=grid_changes)
--- server/schema.py
+++ server/schema.py
@@ -8,13 +8,11 @@ from jwtberry.permission import IsAuthenticated
 from jwtberry.types import JwtAuthResponse
 
 from account.types import UserType, UserInput
-from games.checkers.types import CheckersBoardType
 from games.millionaire.mutations import MillionaireMutation
 from games.millionaire.types import MillionaireQuery
-from games.types import GameType
-from games.checkers.mutations import create_board, update_board_in_game
+from games.checkers.mutations import CheckersMutation
+from games.checkers.types import CheckersQuery
 from core.json import JSON
-from games.checkers.game import get_board
 
 
 import games.bazarblot.types
@@ -28,21 +26,27 @@ class Query(games.bazarblot.types.BazarBlotQuery):
     @strawberry.django.field()
     def millionaire(self) -> MillionaireQuery:
         return MillionaireQuery()
-
-    checkers: List[CheckersBoardType] = strawberry.django.field()
-
-    @strawberry.django.field(permission_classes=[IsAuthenticated])
-    def games(self, info) -> List[GameType]:
-        return GameType.all(info)
-
-    @strawberry.field(permission_classes=[IsAuthenticated])
-    def me(self, info) -> UserType:
-        return info.context.user
     
-    # get board state
-    @strawberry.field
-    def resolve_board_state(self, info, guid: str) -> JSON:
-        return get_board(guid)
+
+    # added queries by Samvel
+    @strawberry.django.field()
+    def checkers(self) -> CheckersQuery:
+        return CheckersQuery()
+    
+    # checkers: List[CheckersBoardType] = strawberry.django.field()
+
+    # @strawberry.django.field(permission_classes=[IsAuthenticated])
+    # def games(self, info) -> List[GameType]:
+    #     return GameType.all(info)
+
+    # @strawberry.field(permission_classes=[IsAuthenticated])
+    # def me(self, info) -> UserType:
+    #     return info.context.user
+    
+    # # get board state
+    # @strawberry.field
+    # def resolve_board_state(self, info, guid: str) -> JSON:
+    #     return get_board(guid)
 
 
 @strawberry.type
@@ -51,12 +55,14 @@ class Mutation:
     # refresh: JwtAuthResponse = refresh_token
     logout = auth.logout()
     register: UserType = auth.register(UserInput)
-    create_board: JSON = create_board
-    update_board_in_game: JSON = update_board_in_game
 
     @strawberry.django.field()
     def millionaire(self) -> MillionaireMutation:
         return MillionaireMutation()
+
+    @strawberry.django.field()
+    def checkers(self) -> CheckersMutation:
+        return CheckersMutation()
 
 
 schema = strawberry.Schema(

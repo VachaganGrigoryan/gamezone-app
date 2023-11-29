0cc41ba1a6df242225935d4c3fc829893cd877d7
saamo24 <sabrahamyan.004@gmail.com>
2023-11-29 20:45:31 +0400

    Added models for  app

--- chat/models.py
+++ chat/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-# from django.contrib.postgres.fields import ArrayField
 
 class Chat(models.Model):
 
     guid = models.UUIDField(auto_created=True, unique=True, db_index=True)
-    game_id = models.ForeignKey('games.Game', on_delete=models.CASCADE)
+    game = models.ForeignKey('games.Game', on_delete=models.CASCADE) 
+    game_guid = models.UUIDField(db_index=True, unique=True)
     members = models.ManyToManyField(
         'account.User',
         verbose_name=_('Members')
     )
-    created_at = models.DateTimeField(auto_now_add=True) #created at
+    created_at = models.DateTimeField(auto_now_add=True)
     
     class Meta:
         db_table = 'chat'
@@ -18,20 +18,15 @@ class Chat(models.Model):
         verbose_name_plural = _('Chats')
 
 
-# Message model 
-# sender -> message model
-# send date
-
 class Message(models.Model):
 
     guid = models.UUIDField(auto_created=True, unique=True, db_index=True)
     sender = models.ForeignKey('account.User', on_delete=models.CASCADE, verbose_name=_('Sender'))
     message = models.TextField(max_length=255)
-    is_read = models.BooleanField(default=False) # message model 
+    is_read = models.BooleanField(default=False)
     sent_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = 'message'
         verbose_name = _('Message')
         verbose_name_plural = _('Messages')
-

e037547196153e265cc3a5b7bed308e1bccb8d58
Vachagan <vachagan.grigoryan@outlook.com>
2023-12-30 00:06:00 +0400

    improve chat models

--- chat/models.py
+++ chat/models.py
@@ -1,14 +1,24 @@
+import uuid
+
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-class Chat(models.Model):
 
-    guid = models.UUIDField(auto_created=True, unique=True, db_index=True)
+class Chat(models.Model):
+    guid = models.UUIDField(
+        default=uuid.uuid4,
+        primary_key=True,
+        editable=False,
+        unique=True,
+        verbose_name=_('GUID'),
+        help_text=_('The unique identifier.'),
+    )
     game = models.ForeignKey('games.Game', on_delete=models.CASCADE) 
     game_guid = models.UUIDField(db_index=True, unique=True)
     members = models.ManyToManyField(
         'account.User',
-        verbose_name=_('Members')
+        verbose_name=_('Members'),
+        through='ChatMembers',
     )
     created_at = models.DateTimeField(auto_now_add=True)
     
@@ -18,9 +28,29 @@ class Chat(models.Model):
         verbose_name_plural = _('Chats')
 
 
-class Message(models.Model):
+class ChatMembers(models.Model):
+    chat = models.ForeignKey(
+        Chat,
+        on_delete=models.CASCADE,
+    )
+    user = models.ForeignKey(
+        'account.User',
+        on_delete=models.CASCADE,
+    )
 
-    guid = models.UUIDField(auto_created=True, unique=True, db_index=True)
+    class Meta:
+        unique_together = ('chat', 'user')
+
+
+class Message(models.Model):
+    guid = models.UUIDField(
+        default=uuid.uuid4,
+        primary_key=True,
+        editable=False,
+        unique=True,
+        verbose_name=_('GUID'),
+        help_text=_('The unique identifier.'),
+    )
     sender = models.ForeignKey('account.User', on_delete=models.CASCADE, verbose_name=_('Sender'))
     message = models.TextField(max_length=255)
     is_read = models.BooleanField(default=False)
